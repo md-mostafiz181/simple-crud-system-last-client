44ff6b352cb1e7f0175cffc7ffa100b9ce3ec0ae
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-25 10:39:44 +0600

    delete specific user by using delete method

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,11 +3,24 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const users = useLoaderData()
+    const handleDeleteUser = _id =>{
+        console.log(_id)
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: "DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.deletedCount<0){
+                alert("user delete successfully")
+            }
+        })
+    }
     return (
         <div>
             <h1>All users:</h1>
             {
-                users.map(user=> <p key={user._id}>{user.name}: {user.email}</p> )
+                users.map(user=> <p key={user._id}>{user.name}: {user.email}: {user._id} <button onClick={()=>handleDeleteUser(user._id)}>X</button></p> )
             }
         </div>
     );
