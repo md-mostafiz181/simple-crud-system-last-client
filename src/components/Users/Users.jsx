a57ce64235d5819ff389708929119d89f7b71c92
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-25 11:00:02 +0600

    deleted successfully

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
-    const users = useLoaderData()
+
+
+    const loadedUsers = useLoaderData()
+    const [users,setUsers]=useState(loadedUsers)
+
+
+
     const handleDeleteUser = _id =>{
         console.log(_id)
         fetch(`http://localhost:5000/users/${_id}`,{
@@ -11,8 +18,10 @@ const Users = () => {
         .then(res=>res.json())
         .then(data=>{
             console.log(data)
-            if(data.deletedCount<0){
+            if(data.deletedCount>0){
                 alert("user delete successfully")
+                const remaining = users.filter(user => user._id !== _id)
+                setUsers(remaining)
             }
         })
     }
